372d73509c64028c2565c38bd7c03e22f889693e
dtdevs25 <dtdevs25@gmail.com>
2026-06-04 21:21:49 -0300

    fix: remove prisma pg adapter to use native query engine

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL!
-
-  const adapter = new PrismaPg({ connectionString })
-
   return new PrismaClient({
-    adapter,
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
   })
 }
